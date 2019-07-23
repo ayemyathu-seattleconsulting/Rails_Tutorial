ce05c1e7f49380fd2309753ee6ff95e71d160c40
Aye Mya Thu <ayemyathu@seattleconsulting.co.jp>
2019-07-23 12:32:19 +0900

    activate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_19_064050) do
+ActiveRecord::Schema.define(version: 2019_07_19_094140) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 2019_07_19_064050) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
   end
 
 end
